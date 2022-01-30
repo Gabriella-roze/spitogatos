6e8c003bb358aa526b13cd94a9b0c84b5e07ba72
Gintare <g.rozenaite@gmail.com>
2022-01-30 15:33:53 +0200

    add responsive layout

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
+const burgerMenu = document.getElementById('burgerMenu')
+const xMenu = document.getElementById('x-menu')
+const navigationWrapper = document.querySelector('.navigation-wrapper')
+const body = document.querySelector('body')
 
+burgerMenu.addEventListener('click', toggleMobileMenu)
+xMenu.addEventListener('click', toggleMobileMenu)
+
+function toggleMobileMenu() {
+  navigationWrapper.classList.toggle('hidden')
+  body.classList.toggle('scroll-disabled')
+}
 
