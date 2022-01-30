939495eecfd1bfde3e42b7ffe7a0c485e19cf923
Gintare <g.rozenaite@gmail.com>
2022-01-30 18:00:09 +0200

    add mobile navigation

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const burgerMenu = document.getElementById('burgerMenu')
 const xMenu = document.getElementById('x-menu')
+const mobileNavHeader = document.querySelector('.mobile-nav-header')
+const mobileNavFooter = document.querySelector('.mobile-nav-footer')
 const navigationWrapper = document.querySelector('.navigation-wrapper')
 const body = document.querySelector('body')
 
@@ -8,6 +10,8 @@ xMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleMobileMenu() {
   navigationWrapper.classList.toggle('hidden')
+  mobileNavHeader.classList.toggle('hidden')
+  mobileNavFooter.classList.toggle('hidden')
   body.classList.toggle('scroll-disabled')
 }
 
