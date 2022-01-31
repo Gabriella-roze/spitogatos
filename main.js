38cb3e44beacbccdd7a8ab3aeff1b2583d8f37f4
Gintare <g.rozenaite@gmail.com>
2022-01-31 21:20:38 +0200

    add form validation

--- main.js
+++ main.js
@@ -1,17 +1,106 @@
-const burgerMenu = document.getElementById('burgerMenu')
-const xMenu = document.getElementById('x-menu')
-const mobileNavHeader = document.querySelector('.mobile-nav-header')
-const mobileNavFooter = document.querySelector('.mobile-nav-footer')
-const navigationWrapper = document.querySelector('.navigation-wrapper')
-const body = document.querySelector('body')
+// VARIABLES
+const burgerMenu = document.getElementById('burgerMenu');
+const xMenu = document.getElementById('x-menu');
+const mobileNavHeader = document.querySelector('.mobile-nav-header');
+const mobileNavFooter = document.querySelector('.mobile-nav-footer');
+const navigationWrapper = document.querySelector('.navigation-wrapper');
+const body = document.querySelector('body');
+const categorySelect = document.querySelector("#inputCategory");
+const subCategorySelect = document.querySelector("#inputSubcategory");
 
-burgerMenu.addEventListener('click', toggleMobileMenu)
-xMenu.addEventListener('click', toggleMobileMenu)
+// LISTENERS
+ categorySelect.addEventListener("change", handleCategoryChange);
+ subCategorySelect.addEventListener("change", handleSubcategoryChance);
+ burgerMenu.addEventListener('click', toggleMobileMenu);
+
+(function init() {
+  fetch("https://run.mocky.io/v3/0b8fbded-6ce4-4cb2-bf2f-d2c39207506b")
+    .then((response) => response.json())
+    .then((data) => populateCategorySelect(data))
+    .catch((error) => {
+      console.warn(error);
+      // Here we could handle the error by logging in somewhere
+      // and showing a message to the user.
+    });
+})();
 
 function toggleMobileMenu() {
+  if (navigationWrapper.classList.contains('hidden')) {
+
+    xMenu.addEventListener('click', toggleMobileMenu)
+  } else {
+    xMenu.removeEventListener('click', toggleMobileMenu)
+  }
   navigationWrapper.classList.toggle('hidden')
   mobileNavHeader.classList.toggle('hidden')
   mobileNavFooter.classList.toggle('hidden')
   body.classList.toggle('scroll-disabled')
+}
+
+function populateCategorySelect(options) {
+  options.forEach(item => {
+    // Create and prepare an option element
+    const option = document.createElement("option");
+    option.value = item.categoryId;
+    option.text = item.name;
+
+    // Also set the subcategories for each option as a data attribute.
+    // We need this to populate the subcategory select later.
+    // We stringify it so it can be saved as a string in the data attribute and parsed later.
+    option.setAttribute(
+      "data-subcategories",
+      item.subCategories ? JSON.stringify(item.subCategories) : ""
+    );
+
+    // Append the option to the select element
+    categorySelect.appendChild(option);
+  });
+}
+
+function populateSubcategorySelect(options) {
+  // First clear the existing options
+  while (subCategorySelect.options.length > 0) {
+    subCategorySelect.remove(0);
+  }
+
+  // Add the default (placeholder) option cause the previous loop removed everything
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.text = "Subcategory";
+  defaultOption.disabled = true;
+  defaultOption.selected = true;
+  defaultOption.hidden = true;
+  subCategorySelect.appendChild(defaultOption);
+
+  // Then populate the select element with the new options
+  options.forEach(item => {
+    const option = document.createElement("option");
+    option.value = item.categoryId;
+    option.text = item.name;
+
+    subCategorySelect.appendChild(option);
+  });
+}
+
+function handleCategoryChange(e) {
+  console.log("[onCategoryChange] Subcategory changed");
+  const currentlySelectedOption = e.target.options[e.target.selectedIndex];
+  const subCategories = currentlySelectedOption.dataset.subcategories;
+
+  if (subCategories) {
+    // If there are subcategories, we parse them to turn them into an array again
+    // and call the populateSubcategorySelect function to populate the subcategories select.
+    const subCategoriesArray = JSON.parse(subCategories);
+    populateSubcategorySelect(subCategoriesArray);
+  } else {
+    populateSubcategorySelect([]);
+  }
+}
+
+function handleSubcategoryChance(e) {
+  console.log("[onSubcategoryChange] Subcategory changed");
+  console.log(e);
+  console.log(e.target.value);
+  console.log(e.target.options[e.target.selectedIndex].text);
 }
 
---
+++ validation.js
@@ -0,0 +1,118 @@
+// VARIABLES
+const form = document.getElementById('contactForm')
+const message = document.getElementById("inputMessage")
+const messageHelpText = document.querySelector("#messageHelpText")
+const checkboxHelpText = document.getElementById('checkboxHelpText')
+
+// LISTENERS & FUNCTIONS
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    if  (validateForm()) {
+        // If the form validatin is passed:
+        // we reset all form errors
+        resetAllFormErrors()
+        
+        // Then here we would submit the form and
+        // show "success" message if that went through
+
+        // and finally reset the form input values
+        form.reset()
+    }
+})
+
+message.addEventListener('input', (e) => {
+    const messageText = e.target.value;
+  
+    // Update the count
+    messageHelpText.textContent = `${messageText.length}/100`
+  
+    // Indicate error if the message is too long
+    if (messageText.length > 100) {
+        message.classList.add("border-error")
+        messageHelpText.classList.add("text-error")
+    } else {
+        message.classList.remove("border-error")
+        messageHelpText.classList.remove("text-error")
+    }
+  });
+
+function resetAllFormErrors() {
+    // Select all inputs with an error
+    let inputsWithErrorArr = document.querySelectorAll('.border-error')
+
+    // loop though the array and remove error messages and error classes
+    inputsWithErrorArr.forEach((input) => {
+        input.classList.remove("border-error")
+        input.nextElementSibling.innerHTML = "This field is required"
+        input.nextElementSibling.classList.remove('text-error')
+    })
+}
+
+function validateForm() {
+    let error = false
+    let min, max
+    document.querySelectorAll("[data-validate]").forEach(element => {
+        element.classList.remove('input-error')
+        switch (element.getAttribute("data-validate")) {
+            case "phone":
+                const phoneRegex = /^[0-9]{3}(-|.)[0-9]{4}(-|.)[0-9]{4}$/
+                input = element.value
+                if (!phoneRegex.test(element.value)) {
+                    console.log("Error in phone nr")
+                    element.classList.add('border-error')
+                    element.nextElementSibling.innerHTML = "Please provide a correctly formatted phone number (123-1234-1234)"
+                    element.nextElementSibling.classList.add('text-error')
+                    error = true
+                }
+                if (!element.value) {
+                    element.classList.add('border-error')
+                    element.nextElementSibling.innerHTML = "This field is required"
+                    element.nextElementSibling.classList.add('text-error')
+                    error = true
+                }
+                break
+            case "name":
+                const allowedChar = /^[a-zA-Z\s]*$/
+                if (!element.value.match(allowedChar)) {
+                    element.classList.add('border-error')
+                    element.nextElementSibling.innerHTML = "You can only type in letters"
+                    element.nextElementSibling.classList.add('text-error')
+                    error = true
+                }
+                if (!element.value) {
+                    element.classList.add('border-error')
+                    element.nextElementSibling.innerHTML = "This field is required"
+                    element.nextElementSibling.classList.add('text-error')
+                    error = true
+                }
+                break
+            case "email":
+                const reg = /^[a-z0-9]+[\._]?[a-z0-9]+@spitogatos.gr/
+                if (!reg.test(element.value.toLowerCase())) {
+                    element.classList.add('border-error')
+                    element.nextElementSibling.innerHTML = "You must add a valid email (...@spitogatos.gr)"
+                    element.nextElementSibling.classList.add('text-error')
+                    error = true
+                }
+                if (!element.value) {
+                    element.classList.add('border-error')
+                    element.nextElementSibling.innerHTML = "This field is required"
+                    element.nextElementSibling.classList.add('text-error')
+                    error = true
+                }
+                break
+            case "message":
+                max = parseInt(element.getAttribute("data-max"))
+                message = element.value
+                if ( message.length > max ) {
+                    element.classList.add('border-error')
+                    element.nextElementSibling.classList.add('text-error')
+                    error = true
+                }
+                break
+        }
+    })
+
+    return error ? false : true
+}
